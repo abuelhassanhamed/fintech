c578feb53feb1248732c2fb72e2f06e4bfaed5ca
aboelhassan <a.elrian@gmail.com>
2024-05-29 19:18:25 +0300

    intial parent

---
+++ fintech-parent/src/main/java/com/beshara/fintech/FintechParentApplication.java
@@ -0,0 +1,13 @@
+package com.beshara.fintech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintechParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FintechParentApplication.class, args);
+	}
+
+}
---
+++ fintech-parent/src/test/java/com/beshara/fintech/FintechParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beshara.fintech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintechParentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
