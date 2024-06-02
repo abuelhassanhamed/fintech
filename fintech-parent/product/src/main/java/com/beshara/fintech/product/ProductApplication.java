bf97718e689411beb854e84101fc9146b8e879b9
aboelhassan <a.elrian@gmail.com>
2024-06-02 19:19:02 +0300

    initial product project

--- fintech-parent/product/src/main/java/com/beshara/fintech/product/ProductApplication.java
+++ fintech-parent/product/src/main/java/com/beshara/fintech/product/ProductApplication.java
@@ -1,13 +1,13 @@
-package com.beshara.fintech.gateway;
+package com.beshara.fintech.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GatewayApplication {
+public class ProductApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GatewayApplication.class, args);
+		SpringApplication.run(ProductApplication.class, args);
 	}
 
 }
---
+++ fintech-parent/product/src/test/java/com/beshara/fintech/product/ProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beshara.fintech.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
