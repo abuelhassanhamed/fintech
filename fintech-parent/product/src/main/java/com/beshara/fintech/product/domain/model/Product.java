cd24141d55a22b976fd2677779b5c3b4bb504c2c
aboelhassan <a.elrian@gmail.com>
2024-06-06 01:50:17 +0300

    empty docker files

---
+++ fintech-parent/product/src/main/java/com/beshara/fintech/product/domain/model/Product.java
@@ -0,0 +1,30 @@
+package com.beshara.fintech.product.domain.model;
+
+public class Product {
+	
+	private String id;
+	private String description;
+	private Long price;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Long getPrice() {
+		return price;
+	}
+	public void setPrice(Long price) {
+		this.price = price;
+	}
+	
+	
+
+}
